71d2e3a911bc71a1a6c5a0e91330aa6b382e84e6
BoscoZhangers <168551399+BoscoZhangers@users.noreply.github.com>
2025-10-19 15:35:51 -0400

    Fixed compile script to match new repo name.

--- compile.sh
+++ compile.sh
@@ -3,7 +3,7 @@
 echo "Running the compile script..."
 cd ~/Desktop/ICS4U1
 javac -cp .:arc.jar RPG.java
-cd ~/Desktop/PokemonRPG
+cd ~/Desktop/PocketRPG
 cp ~/Desktop/ICS4U1/RPG.class .
 
 echo "Successfully compiled and moved binary RPG.class into current directory."
